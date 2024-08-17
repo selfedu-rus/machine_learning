431e8a91cb647f2111e5613ee020ea9b2541a781
selfedu-rus <65663799+selfedu-rus@users.noreply.github.com>
2024-08-17 09:25:37 +0400

    Update machine_learning_16.py

--- machine_learning_16.py
+++ machine_learning_16.py
@@ -1,7 +1,6 @@
 # Пример реализации наивного гауссовского байесовского классификатора
 
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 x_train = np.array([[10, 50], [20, 30], [25, 30], [20, 60], [15, 70], [40, 40], [30, 45], [20, 45], [40, 30], [7, 35]])
@@ -11,15 +10,25 @@ mw1, ml1 = np.mean(x_train[y_train == 1], axis=0)
 mw_1, ml_1 = np.mean(x_train[y_train == -1], axis=0)
 
 # формула для вычисления дисперсии здесь немного другая 1/N*sum(...)
-sw1, sl1 = np.var(x_train[y_train == 1], axis=0)
-sw_1, sl_1 = np.var(x_train[y_train == -1], axis=0)
+sw1, sl1 = np.var(x_train[y_train == 1], axis=0, ddof=1)
+sw_1, sl_1 = np.var(x_train[y_train == -1], axis=0, ddof=1)
 
 print('МО: ', mw1, ml1, mw_1, ml_1)
 print('Дисперсии:', sw1, sl1, sw_1, sl_1)
 
-x = [40, 10]  # длинв, ширина жука
-a_1 = lambda x: -(x[0] - ml_1) ** 2 / (2 * sl_1) - (x[1] - mw_1) ** 2 / (2 * sw_1)
-a1 = lambda x: -(x[0] - ml1) ** 2 / (2 * sl1) - (x[1] - mw1) ** 2 / (2 * sw1)
-y = np.argmax([a_1(x), a1(x)])
+x = [10, 40]  # ширина, длина жука
 
-print('Номер класса (0 - гусеница, 1 - божья коровка): ', y)
+a_1 = lambda x: -np.log(sw_1 * sl_1) - (x[0] - mw_1) ** 2 / (2 * sw_1) - (x[1] - ml_1) ** 2 / (2 * sl_1)
+a1 = lambda x: -np.log(sw1 * sl1) - (x[0] - mw1) ** 2 / (2 * sw1) - (x[1] - ml1) ** 2 / (2 * sl1)
+y = np.argmax([a_1(x), a1(x)]) * 2 - 1
+
+print('Номер класса (-1 - гусеница, 1 - божья коровка): ', y)
+
+# тестирование по обучающей выборки
+pr = []
+for x in x_train:
+    pr.append(np.argmax([a_1(x), a1(x)]) * 2 - 1)
+
+pr = np.array(pr)
+Q = np.mean(pr != y_train) # доля ошибок
+print(Q)
